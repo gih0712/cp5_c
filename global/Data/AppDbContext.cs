7319000521732823adca28f0f02aaed1ce0cad6c
Giulia Camillo <giulia15camillo@gmail.com>
2025-09-02 10:48:27 -0300

    Update AppDbContext.cs

--- global/Data/AppDbContext.cs
+++ global/Data/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using global.Models;
 
 namespace global.Data
@@ -9,16 +9,12 @@ namespace global.Data
             : base(options)
         {
         }
+
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Sensor> Sensores { get; set; }
         public DbSet<PrevisaoTempo> Previsoes { get; set; }
         public DbSet<LeituraSensor> LeituraSensores { get; set; }
         public DbSet<Alerta> Alertas { get; set; }
         public DbSet<LogApi> LogsApi { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
     }
 }
